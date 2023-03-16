acb4e84be22098e3442bc4165f793325bcdb0d1a
RikuVesanto <riku.vesanto@gmail.com>
2023-03-16 20:02:22 +0200

    add: getData(requestUrl, callbacks)

--- src/utils/http-requests.js
+++ src/utils/http-requests.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 /**
  * Execute http request promise and then proceed with the callback functions.
  * @param {*} request Request promise
@@ -15,4 +17,13 @@ async function executeRequestCallbacks(request, callbacks) {
 				callbacks.onError(error.response)
 			} catch (callbackError) {}
 		})
+}
+
+/**
+ * Get data and execute the given callback based on the result.
+ * @param {*} requestUrl The specific route that the request is made to.
+ * @param {*} callbacks Object with optional callback functions named onSuccess, onError and onCompletion.
+ */
+async function getData(requestUrl, callbacks) {
+	await executeRequestCallbacks(axios.get(requestUrl), callbacks)
 }
