e24217cc99498695c46502d7aa219d0a3ba77ebf
RikuVesanto <riku.vesanto@gmail.com>
2023-03-18 14:08:33 +0200

    add: getFiveThreeHourForecasts(lat, lon)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { getData } from './utils/http-requests'
-import { kelvinToCelsius, getNumbersSuffix } from './utils/general-functions'
+import {
+	limitFunction,
+	kelvinToCelsius,
+	getNumbersSuffix,
+} from './utils/general-functions'
 import WeatherDisplay from './components/WeatherDisplay'
 
 function App() {
 	const [currentWeather, setCurrentWeather] = useState<any>(null)
+	const [threeHourForecasts, setThreeHourForecasts] = useState<any>(null)
 
 	useEffect(() => {
 		getCurrentWeather('61.4991', '23.7871')
+		getFiveThreeHourForecast('61.4991', '23.7871')
 	}, [])
 
 	async function getCurrentWeather(lat: String, lon: String) {
@@ -15,7 +21,6 @@ function App() {
 			`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`,
 			{
 				onSuccess: async (response: any) => {
-					console.log(response)
 					const data = response.data
 					const dateTime = new Date(data.dt * 1000)
 					const day = dateTime.getDate()
@@ -37,6 +42,36 @@ function App() {
 						iconId: data.weather[0].icon,
 					}
 					setCurrentWeather(parsedWeather)
+				},
+				onError: (error: Error) => {
+					console.log(error)
+				},
+			}
+		)
+	}
+
+	async function getFiveThreeHourForecast(lat: String, lon: String) {
+		return await getData(
+			`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`,
+			{
+				onSuccess: async (response: any) => {
+					const condenceForecastObject = (forecast: any) => ({
+						time:
+							(new Date(forecast.dt * 1000).getHours() < 10 ? '0' : '') +
+							new Date(forecast.dt * 1000).getHours().toString() +
+							':00',
+						temperature: Math.round(kelvinToCelsius(forecast.main.temp)),
+						windSpeed: forecast.wind.speed,
+						humidity: forecast.main.humidity,
+						rain: 'rain' in forecast ? forecast.rain['1h'] : 0,
+						iconId: forecast.weather[0].icon,
+					})
+					const getFiveForecasts = limitFunction(condenceForecastObject, 5)
+					const forecasts = response.data.list
+						.map((forecast: any) => getFiveForecasts(forecast))
+						.filter((forecast: any) => forecast != null)
+					console.log(forecasts)
+					setThreeHourForecasts(forecasts)
 				},
 				onError: (error: Error) => {
 					console.log(error)
